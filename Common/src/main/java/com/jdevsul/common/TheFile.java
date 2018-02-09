c90af0e76a3ac00184d6db54e4de1b5ca1f9171d
engsulta <en.ahmedsultan@gmail.com>
2018-02-09 15:07:48 +0200

    the file class no problems

--- Common/src/main/java/com/jdevsul/common/TheFile.java
+++ Common/src/main/java/com/jdevsul/common/TheFile.java
@@ -10,5 +10,41 @@ package com.jdevsul.common;
  * @author sulta
  */
 class TheFile {
+    byte[] file;
+    String name;
+    int from ;
+    int to;
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
     
 }
