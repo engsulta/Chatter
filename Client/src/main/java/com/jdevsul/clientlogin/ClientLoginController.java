3960599f0bb57efe9ec5cbe448a7e44dc351cfc3
engsulta <en.ahmedsultan@gmail.com>
2018-02-13 16:20:33 +0200

    modifi sulta_13

--- Client/src/main/java/com/jdevsul/clientlogin/ClientLoginController.java
+++ Client/src/main/java/com/jdevsul/clientlogin/ClientLoginController.java
@@ -55,7 +55,9 @@ public class ClientLoginController implements Initializable {
             Logger.getLogger(ClientLoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-////modi
+////////////
+    
+    
     @FXML
     private void HandleLoginAction(ActionEvent event) {
 
