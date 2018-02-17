d56c8140ba29f76e624063ffa70062e8ded43a6d
EmanFikry <emanfikry2014@gmail.com>
2018-02-17 12:36:25 +0200

    contactlist controller

--- Client/src/main/java/com/jdevsul/clientContacts/MyContactListController.java
+++ Client/src/main/java/com/jdevsul/clientContacts/MyContactListController.java
@@ -10,6 +10,7 @@ import com.jdevsul.DBclasses.Group;
 import com.jdevsul.clientFriendRequest.FriendRequestController;
 import com.jdevsul.clientimp.ClientImpl;
 import com.jdevsul.interfaces.ServerManagerInt;
+import java.io.IOException;
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -27,9 +28,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
@@ -37,7 +41,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -59,7 +65,7 @@ public class MyContactListController implements Initializable {
     ClientImpl clientImpl;
 
     public void updateMyContacts(ArrayList<Client> contacts) {
-        //display online friends first
+
         Collections.sort(contacts, new Comparator<Client>() {
             @Override
             public int compare(Client c1, Client c2) {
@@ -67,111 +73,89 @@ public class MyContactListController implements Initializable {
             }
         });
         ObservableList<Client> myContacts = FXCollections.observableArrayList(contacts);
-
         //add the contacts to the listView
         myContactList.setItems(myContacts);
-        Vector<String> ids = new Vector();
         //make custom render --> updateItem
         myContactList.setCellFactory(new Callback<ListView<Client>, ListCell<Client>>() {
             @Override
             public ListCell<Client> call(ListView<Client> param) {
                 return new ListCell<Client>() {
                     @Override
-                    protected void updateItem(final Client myContact, boolean empty) {
-                        super.updateItem(myContact, empty);
+                    protected void updateItem(Client myContact, boolean empty) {
+                        try {
+                            super.updateItem(myContact, empty);
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("CustomList.fxml"));
 
-                        //containerVBox include all boxes
-                        VBox containerVBox = new VBox();
+                            Pane parent = loader.load();
+                            Circle contactImage = (Circle) parent.getChildren().get(0);
+                            Circle contactOnline = (Circle) parent.getChildren().get(1);
 
-                        //info include images, name
-                        HBox info = null;
+                            Label contactStatus = (Label) parent.getChildren().get(3);
+                            Label contactName = (Label) parent.getChildren().get(2);
 
-                        //status include away/available/busy
-                        HBox status = null;
+                            Image image = null;
+                            ImagePattern myimImagePattern = null;
+                            if (myContact != null && !empty) {
 
-                        //images include image, online/offline
-                        HBox images = null;
+                                //all client images are on folder on server
+                                //getClientImage returns the path of the image
+                                image = new Image(myContact.getClientImage(), 50, 50, false, false);
+                        //          image = new Image("clientjavafx2rmi/offline.png", 50, 50, false, false);
+                                myimImagePattern = new ImagePattern(image);
 
-                        //name include client name
-                        HBox name = null;
+                                contactImage.setFill(myimImagePattern);
+                                contactImage.setRadius(25);
+                                contactImage.setVisible(true);
 
-                        Image image = null;
-                        ImagePattern myimImagePattern = null;
-                        Circle contactImage = null;
+                                if (myContact.getClientOnline() == 0) {
+                                    contactOnline.setFill(Color.GREEN);
+                                } else {
+                                    contactOnline.setFill(Color.RED);
+                                }
+                                contactName.setText(myContact.getClientName());
 
-                        ImageView contactOnline = null;
-
-                        Text contactName = null;
-                        //available, away, busy
-                        Text contactStatus = null;
-
-                        if (myContact != null && !empty) {
-
-                            info = new HBox();
-                            status = new HBox();
-                            images = new HBox();
-                            name = new HBox();
-
-                            //all client images are on folder on server
-                            //getClientImage returns the path of the image
-                            image = new Image(myContact.getClientImage(), 50, 50, false, false);
-                            myimImagePattern = new ImagePattern(image);
-                            contactImage = new Circle();
-                            contactImage.setFill(myimImagePattern);
-                            contactImage.setRadius(25);
-                            contactImage.setVisible(true);
-
-                            if (myContact.getClientOnline() == 0) {
-                                contactOnline = new ImageView("clientjavafx2rmi/online.png");
-                            } else {
-                                contactOnline = new ImageView("clientjavafx2rmi/offline.png");
-                            }
-                            contactName = new Text(myContact.getClientName());
-
-                            //when the user press done after choosing his/her group
-//                            done.setOnAction(new EventHandler<ActionEvent>() {
-//                                @Override
-//                                public void handle(ActionEvent event) {
+                                //when the user click on the vbox anywhere
+                                /* containerVBox.setOnMousePressed(new EventHandler<MouseEvent>() {
+    
+    @Override
+    public void handle(MouseEvent event) {
+    
+    //   test1.setText(String.valueOf(myContact.getClientID()));
+    }
+    });*/
+                                //when the user press done after choosing his/her group
+//                                done.setOnAction(new EventHandler<ActionEvent>() {
+//                                    @Override
+//                                    public void handle(ActionEvent event) {
 //
-//                                    ObservableList<Client> selectedItems = myContactList.getSelectionModel().getSelectedItems();
-//                                    Group group = new Group();
-//                                    group.setClientID(myContact.getClientID());
+//                                        ObservableList<Client> selectedItems = myContactList.getSelectionModel().getSelectedItems();
+//                                        Group group = new Group();
+//                                        group.setClientID(myContact.getClientID());
 //
-//                                    ArrayList<Client> selectedClients = new ArrayList<>();
-//                                    for (Client client : selectedItems) {
-//                                        selectedClients.add(client);
+//                                        ArrayList<Client> selectedClients = new ArrayList<>();
+//                                        selectedItems.forEach((client) -> {
+//                                            selectedClients.add(client);
+//                                        });
+//
+//                                        String test = "";
+//                                        for (Client c : selectedItems) {
+//                                            test += String.valueOf(c.getClientID());
+//                                        }
+//                                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//                                        alert.setTitle("Information Dialog");
+//                                        alert.setHeaderText("I have a great message for you!");
+//                                        alert.setContentText(String.valueOf(test));
+//
+//                                        alert.showAndWait();
 //                                    }
-//                                }
-//                            });
-                            contactStatus = new Text(myContact.getClientStatus());
-
-                            contactStatus.setTextAlignment(TextAlignment.CENTER);
-                            contactStatus.setFont(Font.font("Agency FB", FontWeight.BOLD, 17));
-
-                            contactName.setTextAlignment(TextAlignment.CENTER);
-                            contactName.setFont(Font.font("Agency FB", FontWeight.BOLD, 25));
-
-                            contactOnline.setFitWidth(15);
-                            contactOnline.setFitHeight(15);
-
-                            status.getChildren().add(contactStatus);
-                            status.setAlignment(Pos.BASELINE_CENTER);
-
-                            images.getChildren().addAll(contactImage, contactOnline);
-                            images.setAlignment(Pos.BASELINE_LEFT);
-                            images.setSpacing(-10);
-
-                            name.getChildren().add(contactName);
-                            name.setAlignment(Pos.BASELINE_CENTER);
-
-                            info.getChildren().addAll(images, name);
-                            info.setSpacing(40);
-
-                            containerVBox.getChildren().addAll(info, status);
-
-                            setGraphic(containerVBox);
-                        } else {
-                            setGraphic(null);
+//                                });
+                                contactStatus.setText(myContact.getClientStatus());
+                                setGraphic(parent);
+                            } else {
+                                setGraphic(null);
+                            }
+                        } catch (IOException ex) {
+                            Logger.getLogger(MyContactListController.class.getName()).log(Level.SEVERE, null, ex);
                         }
                     }
                 };
